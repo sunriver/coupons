8f4ad7d52d063b4373c70c1065ae0e9c4995b195
lumf <luminfeng@ergengtv.com>
2020-12-02 13:12:30 +0800

    cloud function

--- App.vue
+++ App.vue
@@ -11,7 +11,9 @@ export default {
 	},
 	globalData: {
 		api: {
-			home: 'https://055cfd20-bfe4-4b9a-be9d-f7c2cac59a57.bspapp.com/http/api/home',
+			// home: 'https://github.com/sunriver/sunriver.github.io/blob/main/coupon/home.json'
+			home: 'https://1ca0ef49-15dd-4eaa-89b6-649e70f5cce5.bspapp.com/http/api/home',
+			// home: 'https://055cfd20-bfe4-4b9a-be9d-f7c2cac59a57.bspapp.com/http/api/home',	
 		},
 	}
 
